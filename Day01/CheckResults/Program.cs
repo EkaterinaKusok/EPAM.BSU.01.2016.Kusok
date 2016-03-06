a51b1df11bc4c936c1d4c10908cd1becf769ef2f
EkaterinaKusok <https://github.com/EkaterinaKusok/EPAM.Day1.git>
2016-03-06 04:21:08 +0300

    Task1. Calculate the N-th root of the number of Newton's method

---
+++ Day01/CheckResults/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using N_th_root;
+
+namespace CheckResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3126;
+            int n = 5;
+            Console.Write(MathClass.FindNthRoot(x,n,0.0001));
+            Console.WriteLine();
+            Console.Write(Math.Pow(3126, (double)1/n));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day01/N_th_root/MathClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_th_root
+{
+    public class MathClass
+    {
+        public static double FindNthRoot(double a, int n, double eps = 0.0001)
+        {
+            //корень определяется для натуральных n>=2
+            if (n < 2)
+                throw new FormatException($"Wrong input data: {n}<2!");
+            double x0, x1 = 1;
+            do
+            {
+                x0 = x1;
+                x1 = ((n - 1)*x0 + a/QuickInvolute(x0, n-1))/n;
+            } while (Math.Abs(x1 - x0) > eps);
+            return x1;
+        }
+        public static double QuickInvolute(double x, int n)
+        {
+            if (n < 0)
+                throw new FormatException($"Wrong input data: {n}<0!");
+            if (n == 0)
+                return 1;
+            double z = x;
+            double res = 1;
+            while (n>0)
+            {
+                if (n%2 == 1)
+                    res *= z;
+                z *= z;
+                n /= 2;
+            }
+            return res;
+        }
+    }
+}
