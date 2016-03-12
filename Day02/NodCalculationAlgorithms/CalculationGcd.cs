6f5cd54deb3f2cd6e8f6e31903d91648482ebf8c
EkaterinaKusok <https://github.com/EkaterinaKusok/EPAM.BSU.01.2016.Kusok.01>
2016-03-12 23:00:53 +0300

    GCD Algorithms with timer

--- Day02/NodCalculationAlgorithms/CalculationGcd.cs
+++ Day02/NodCalculationAlgorithms/CalculationGcd.cs
@@ -1,69 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GcdCalculationAlgorithms
 {
     public class CalculationGcd
     {
-        public static int EuclideanAlgorithm(int first, int second, out double workingTime)
+        public static int EuclideanAlgorithm(int first, int second, out TimeSpan workingTime)
         {
-            workingTime = 0;
             if (first < 0 || second < 0)
             {
                 throw new FormatException("At least one of the parameters < 0 !");
             }
-            //* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
-            if (first == 0) return second;
-            if (second == 0) return first;
-            if (first == 1 || second == 1) return 1;
-            
-            while (second != 0)
-                second = first % (first = second);
-            return first;
+            Stopwatch stopWatch = new Stopwatch();
+            try
+            {
+                stopWatch.Start();
+                //* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
+                if (first == 0) return second;
+                if (second == 0) return first;
+                if (first == 1 || second == 1) return 1;
+
+                while (second != 0)
+                    second = first%(first = second);
+                return first;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                workingTime = stopWatch.Elapsed;
+            }
         }
 
-        public static int SteinAlgorithm(int first, int second, out double workingTime)
+        public static int SteinAlgorithm(int first, int second, out TimeSpan workingTime)
         {
-            workingTime = 0;
-            if (first < 1 || second < 1)
+            if (first < 0 || second < 0)
             {
-                throw new FormatException("At least one of the parameters < 1 !");
+                throw new FormatException("At least one of the parameters < 0 !");
             }
-            if (first == 0) return second;
-            if (second == 0) return first;
-            if (first == second) return first;
-            if (first == 1 || second == 1) return 1;
+            Stopwatch stopWatch = new Stopwatch();
+            try
+            {
+                stopWatch.Start();
+                if (first == 0) return second;
+                if (second == 0) return first;
+                if (first == second) return first;
+                if (first == 1 || second == 1) return 1;
 
-            int shift;
-            /* Let shift := lg K, where K is the greatest power of 2
-                  dividing both u and v. */
-            for (shift = 0; ((first | second) & 1) == 0; ++shift)
-            {
-                first >>= 1;
-                second >>= 1;
-            }
-            while ((first & 1) == 0)
-                first >>= 1;
-            /* From here on, first number is always odd. */
-            do
-            {
-                /* remove all factors of 2 in second number -- they are not common */
-                /*   note: second number is not zero, so while will terminate */
-                while ((second & 1) == 0)  /* Loop X */
-                    second >>= 1;
-                /* Now first and second numbers are both odd. Swap if necessary so 
-                    first <= second, then set second = second - first (which is even). */
-                if (first > second)
+                int shift;
+                /* Let shift := lg K, where K is the greatest power of 2
+                  dividing both first and second. */
+                for (shift = 0; ((first | second) & 1) == 0; ++shift)
                 {
-                    int t = second; second = first; first = t;
-                }  // Swap u and v.
-                second = second - first;          // Here first >= second.
-            } while (second != 0);
-            /* restore common factors of 2 */
-            return first << shift;
+                    first >>= 1;
+                    second >>= 1;
+                }
+                while ((first & 1) == 0)
+                    first >>= 1;
+                /* From here on, first number is always odd. */
+                do
+                {
+                    /* remove all factors of 2 in second number -- they are not common */
+                    /*   note: second number is not zero, so while will terminate */
+                    while ((second & 1) == 0) /* Loop X */
+                        second >>= 1;
+                    /* Now first and second numbers are both odd. Swap if necessary so 
+                    first <= second, then set second = second - first (which is even). */
+                    if (first > second)
+                    {
+                        int t = second;
+                        second = first;
+                        first = t;
+                    } // Swap u and v.
+                    second = second - first; // Here first >= second.
+                } while (second != 0);
+                /* restore common factors of 2 */
+                return first << shift;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                workingTime = stopWatch.Elapsed;
+            }
 
         }
     }
