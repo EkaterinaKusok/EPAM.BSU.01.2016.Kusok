e9e3269bb4c638122845dd6c364c0fb8c857a1ef
EkaterinaKusok <https://github.com/EkaterinaKusok/EPAM.BSU.01.2016.Kusok.01>
2016-03-12 23:21:45 +0300

    Add NUnit Tests

---
+++ Day02/CalculationGcdTests/CalculationGcdTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static GcdCalculationAlgorithms.CalculationGcd;
+
+namespace GcdCalculationAlgorithmsTests
+{
+    [TestFixture]
+    public class CalculationGcdTests
+    {
+        private TimeSpan _workingTime;
+        public IEnumerable<TestCaseData> TestData
+        {
+
+            get
+            {
+                yield return new TestCaseData(8, 16).Returns(8);
+                yield return new TestCaseData(8, 0).Returns(8);
+                yield return new TestCaseData(0, 0).Returns(0);
+                yield return new TestCaseData(1, 3).Returns(1);
+                yield return new TestCaseData(0, -1).Throws(typeof(FormatException));
+            }
+        }
+        
+        [Test, TestCaseSource(nameof(TestData))]
+        public double EuclideanAlgorithm_FindGcd2Numbers_WithYield(int first, int second)
+        {
+            return EuclideanAlgorithm(first, second, out _workingTime);
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public double SteinAlgorithm_FindGcd2Numbers_WithYield(int first, int second)
+        {
+            return SteinAlgorithm(first, second, out _workingTime);
+        }
+    }
+}
