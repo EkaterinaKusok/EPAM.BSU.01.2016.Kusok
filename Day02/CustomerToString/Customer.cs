b5d28082579c49123091cba97ee6ee15e9763f78
EkaterinaKusok <ekaterina.kusok@gmail.com>
2016-03-14 01:16:20 +0300

    String representation for Customer

---
+++ Day02/CustomerToString/Customer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CustomerToString
+{
+    public class Customer : IFormattable
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+
+        public Customer(string name, string phone, decimal revenue)
+        {
+            this.Name = name;
+            this.ContactPhone = phone;
+            this.Revenue = revenue;
+        }
+
+        public override string ToString()
+        {
+            return this.ToString("N", null);
+        }
+
+        public string ToString(string format)
+        {
+            return this.ToString(format, null);
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            // Handle null or empty arguments.
+            if (String.IsNullOrEmpty(format)) format = "NPR";
+            // Remove any white space and convert to uppercase.
+            format = format.Trim().ToUpperInvariant();
+
+            if (provider == null) provider = NumberFormatInfo.CurrentInfo;
+
+            switch (format)
+            {
+                case "NPR":
+                    return "CustomerRecord: " + this.Name + ", " + this.ContactPhone + ", " +
+                           this.Revenue.ToString(provider);
+                case "N":
+                    return "CustomerRecord: " + this.Name;
+                case "P":
+                    return "CustomerRecord: " + this.ContactPhone;
+                case "R":
+                    return "CustomerRecord: " + this.Revenue.ToString("0,0", provider);
+                case "R,":
+                    return "CustomerRecord: " + this.Revenue.ToString("0,0", CultureInfo.InvariantCulture);
+                default:
+                    throw new FormatException(String.Format("The '{0}' format string is not supported.", format));
+            }
+        }
+    }
+}
---
+++ Day02/CustomerToStringTests/CustomerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CustomerToString;
+//using static CustomerToString.Customer;
+
+namespace CustomerToStringTests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        public IEnumerable<TestCaseData> TestData
+        {
+
+            get
+            {
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m,"N",null).Returns("CustomerRecord: Jeffrey Rihter");
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "P",null).Returns("CustomerRecord: +1(425)555-0100");
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "NPR",null).Returns("CustomerRecord: Jeffrey Rihter, +1(425)555-0100, 1000000");
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "A",null).Throws(typeof(FormatException));
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "R",null).Returns("CustomerRecord: 1 000 000");
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "R", CultureInfo.CreateSpecificCulture("el-GR")).Returns("CustomerRecord: 1.000.000");
+                yield return new TestCaseData("Jeffrey Rihter", "+1(425)555-0100", 1000000m, "R,", null).Returns("CustomerRecord: 1,000,000");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public string CustomerToString_InputCustomerWithParamerets_WithYield(string name, string phone, decimal revenue, string parameter, IFormatProvider provider)
+        {
+            Customer temp = new Customer(name, phone, revenue);
+            return temp.ToString(parameter, provider);
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public string Customer_FormattedOutput_WithYield(string name, string phone, decimal revenue, string parameter, IFormatProvider provider)
+        {
+            Customer temp = new Customer(name,phone, revenue);
+            return String.Format(provider,"{0:"+parameter+"}", temp);
+        }
+    }
+}
