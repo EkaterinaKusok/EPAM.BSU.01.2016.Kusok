f0bd91c3676ef0b0b9ad673c20b51f4e01e2d3c3
EkaterinaKusok <https://github.com/EkaterinaKusok/EPAM.BSU.01.2016.Kusok.01>
2016-03-13 05:47:09 +0300

    Add formatted output from int to hex and tests

---
+++ Day02/FormattedOutputTests/IntExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static FormattedOutput.IntExtension;
+
+namespace FormattedOutputTests
+{
+    [TestFixture]
+    public class IntExtensionTests
+    {
+        public IEnumerable<TestCaseData> TestData
+        {
+
+            get
+            {
+                yield return new TestCaseData(2147483647).Returns("7FFFFFFF");
+                yield return new TestCaseData(0).Returns("0");
+                yield return new TestCaseData(129300).Returns("1F914");
+                yield return new TestCaseData(-2147483646).Returns("80000002");
+                yield return new TestCaseData(-2147483648).Returns("80000000");
+                yield return new TestCaseData(-2134896640).Returns("80C01000");
+                yield return new TestCaseData(-300).Returns("FFFFFED4");
+
+                //yield return new TestCaseData(new int[] { 0, 20, -1 }).Throws(typeof(ArgumentOutOfRangeException));
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public string ConvertToHex_InputInt_WithYield(int decValue)
+        {
+            return decValue.ConvertToHex();
+        }
+    }
+}
