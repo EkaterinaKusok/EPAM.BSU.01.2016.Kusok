c47cd0e35967dcdda96a0d052b17a13960ebe44a
EkaterinaKusok <ekaterina.kusok@gmail.com>
2016-03-17 16:35:23 +0300

    subsrting for polynomials

--- Day03/PolynomialClass/Polynomial.cs
+++ Day03/PolynomialClass/Polynomial.cs
@@ -32,10 +32,14 @@ namespace PolynomialClass
         {
             if (coefficients.Length == 0 || coefficients == null)
                 return "";
+            else if (coefficients[0].Equals(0) && coefficients.Length == 1)
+                return "0";
             else
             {
                 StringBuilder Expression = new StringBuilder();
-                if(coefficients[0]!=0 && coefficients.Length!=1)Expression.Insert(0, coefficients[0]);
+                
+                if(coefficients[0]!=0 && coefficients.Length!=1) Expression.Insert(0, coefficients[0]);
+
                 for (int i = 1; i < this.coefficients.Length; i++)
                 {
                     if ( coefficients[i - 1] > 0)
@@ -51,25 +55,24 @@ namespace PolynomialClass
             return this.coefficients.GetHashCode();
         }
 
-        //сложение
-        public Polynomial Add(Polynomial right)
+        public Polynomial Add(Polynomial rhs)
         {
             double[] result;
             if (this.coefficients == null || this.coefficients.Length == 0)
-                result = right.coefficients;
-            else if (right.coefficients == null || right.coefficients.Length == 0)
+                result = rhs.coefficients;
+            else if (rhs.coefficients == null || rhs.coefficients.Length == 0)
                 result = this.coefficients;
             else
             {
-                if (coefficients.Length >= right.coefficients.Length)
+                if (coefficients.Length >= rhs.coefficients.Length)
                 {
                     result = coefficients;
-                    for (int i = 0; i < right.coefficients.Length; i++)
-                        result[i] += right.coefficients[i];
+                    for (int i = 0; i < rhs.coefficients.Length; i++)
+                        result[i] += rhs.coefficients[i];
                 }
                 else
                 {
-                    result = right.coefficients;
+                    result = rhs.coefficients;
                     for (int i = 0; i < coefficients.Length; i++)
                     {
                         result[i] += coefficients[i];
@@ -102,22 +105,100 @@ namespace PolynomialClass
             return rhs + lhs;
         }
 
-        // умножение
-        public Polynomial Multiply(Polynomial right)
+        public Polynomial Multiply(Polynomial rhs)
         {
             double[] result;
-            if (this.coefficients == null || right.coefficients == null)
+            if (this.coefficients == null || rhs.coefficients == null)
                 result = null;
-            else if (coefficients.Length == 0 || right.coefficients.Length == 0)
+            else if (coefficients.Length == 0 || rhs.coefficients.Length == 0)
                 result = new double[0] {};
             else
             {
-                result = new double[coefficients.Length + right.coefficients.Length-1];
+                result = new double[coefficients.Length + rhs.coefficients.Length-1];
                 for (int i = 0; i < coefficients.Length; i++)
-                    for (int j = 0; j < right.coefficients.Length; j++)
-                        result[i + j] += coefficients[i]*right.coefficients[j];
+                    for (int j = 0; j < rhs.coefficients.Length; j++)
+                        result[i + j] += coefficients[i]*rhs.coefficients[j];
             }
             return new Polynomial(result);
+        }
+        public static Polynomial operator *(Polynomial lhs, Polynomial rhs)
+        {
+            return lhs.Multiply(rhs);
+        }
+        public static Polynomial operator *(Polynomial lhs, double rhs)
+        {
+            double[] result;
+            if (lhs.coefficients == null)
+                result = null;
+            else if (lhs.coefficients.Length == 0)
+                result = new double[0] { };
+            else if (rhs.Equals(0.0))
+                result=new double[1] {0};
+            else
+            {
+                result = new double[lhs.coefficients.Length];
+                for (int i = 0; i < lhs.coefficients.Length; i++)
+                    result[i] = lhs.coefficients[i] * rhs;
+            }
+            return new Polynomial(result);
+        }
+        public static Polynomial operator *(double lhs, Polynomial rhs)
+        {
+            return rhs * lhs;
+        }
+
+        public Polynomial Substruct(Polynomial rhs)
+        {
+            double[] result;
+            if (this.coefficients == null || this.coefficients.Length == 0)
+                result = rhs.coefficients;
+            else if (rhs.coefficients == null || rhs.coefficients.Length == 0)
+                result = this.coefficients;
+            else
+            {
+                if (coefficients.Length >= rhs.coefficients.Length)
+                {
+                    result = coefficients;
+                    for (int i = 0; i < rhs.coefficients.Length; i++)
+                        result[i] -= rhs.coefficients[i];
+                }
+                else
+                {
+                    result = rhs.coefficients;
+                    for (int i = 0; i < coefficients.Length; i++)
+                    {
+                        result[i] = (-1)*result[i] + coefficients[i];
+                    }
+                    for (int i = coefficients.Length; i < rhs.coefficients.Length; i++)
+                    {
+                        result[i] *= (-1);
+                    }
+                }
+            }
+            return new Polynomial(result);
+        }
+        public static Polynomial operator -(Polynomial lhs, Polynomial rhs)
+        {
+            return lhs.Substruct(rhs);
+        }
+        public static Polynomial operator -(Polynomial lhs, double rhs)
+        {
+            double[] result;
+            if (lhs.coefficients == null || lhs.coefficients.Length == 0)
+            {
+                result = new double[1];
+                result[0] = rhs;
+            }
+            else
+            {
+                result = lhs.coefficients;
+                result[0] -= rhs;
+            }
+            return new Polynomial(result);
+        }
+        public static Polynomial operator -(double lhs, Polynomial rhs)
+        {
+            return (rhs - lhs)*(-1);
         }
     }
 }
--- Day03/PolynomialClassTests/PolynomialTests.cs
+++ Day03/PolynomialClassTests/PolynomialTests.cs
@@ -13,19 +13,95 @@ namespace PolynomialClassTests
     public class PolynomialTests
     {
 
-        public IEnumerable<TestCaseData> TestData
+        public IEnumerable<TestCaseData> TestData1
         {
-
             get
             {
-                yield return new TestCaseData(new Polynomial(new double[]{8.0, 16.0}), new Polynomial(new double[] { 0, 1.0 })).Returns(new Polynomial(new double[]{0.0,8.0,16.0}));
+                yield return new TestCaseData(new Polynomial(new double[]{-8.0, 16.0}), 0.0).Returns(new double[] {0});
+                yield return new TestCaseData(new Polynomial(new double[] { -8.0, 16.0 }), -1.0).Returns(new double[] { 8.0, -16.0 });
             }
         }
-
-        [Test, TestCaseSource(nameof(TestData))]
-        public Polynomial Polynomial_Multiply(Polynomial data1, Polynomial data2)
+        [Test, TestCaseSource(nameof(TestData1))]
+        public double[] Polynomial_MultiplyWithNumber(Polynomial data1, double data2)
         {
-            return data1.Multiply(data2);
+            //return (data1*data2).Get();
+            return (data2*data1).Get();
+        }
+
+        public IEnumerable<TestCaseData> TestData2
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(new double[] { -8.0, 16.0 }), new Polynomial(new double[] { -2.0 })).Returns(new double[] { 16, -32});
+                yield return new TestCaseData(new Polynomial(new double[] { -1, 1 }), new Polynomial(new double[] { 1, 1 })).Returns(new double[] { -1,0,1 });
+            }
+        }
+        [Test, TestCaseSource(nameof(TestData2))]
+        public double[] Polynomial_MultiplyWithPolynomial(Polynomial data1, Polynomial data2)
+        {
+            //return (data1 * data2).Get();
+            return (data2*data1).Get();
+        }
+
+        public IEnumerable<TestCaseData> TestData3
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(new double[] {-8, 16}), 8).Returns(new double[] {0, 16});
+                yield return new TestCaseData(new Polynomial(new double[] {-1, 1}), 0).Returns(new double[] {-1, 1});
+            }
+        }
+        [Test, TestCaseSource(nameof(TestData3))]
+        public double[] Polynomial_AddWithNumber(Polynomial data1, double data2)
+        {
+            return (data1+data2).Get();
+        }
+
+
+        public IEnumerable<TestCaseData> TestData4
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(new double[] { -8.0, 16.0 }), new Polynomial(new double[] { -2.0 })).Returns(new double[] { -10,16 });
+                yield return new TestCaseData(new Polynomial(new double[] { -1, 1 }), new Polynomial(new double[] { 1, 1 })).Returns(new double[] {0,2});
+            }
+        }
+        [Test, TestCaseSource(nameof(TestData4))]
+        public double[] Polynomial_AddWithPolynomial(Polynomial data1, Polynomial data2)
+        {
+            //return (data1 + data2).Get();
+            return (data2 + data1).Get();
+        }
+
+        public IEnumerable<TestCaseData> TestData5
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(new double[] { -8, 16 }), -8).Returns(new double[] { 0, 16 });
+                yield return new TestCaseData(new Polynomial(new double[] { -1, 1 }), 0).Returns(new double[] { -1, 1 });
+            }
+        }
+        [Test, TestCaseSource(nameof(TestData5))]
+        public double[] Polynomial_SubstructWithNumber(Polynomial data1, double data2)
+        {
+            //return (data1 - data2).Get();
+            return ((-1) * (data2 - data1)).Get();
+        }
+
+
+        public IEnumerable<TestCaseData> TestData6
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(new double[] { -8.0, 16.0 }), new Polynomial(new double[] { -8.0 })).Returns(new double[] { 0, 16 });
+                yield return new TestCaseData(new Polynomial(new double[] { -1, 1 }), new Polynomial(new double[] { 1, 1 })).Returns(new double[] {-2, 0 });
+            }
+        }
+        [Test, TestCaseSource(nameof(TestData6))]
+        public double[] Polynomial_SubstructWithPolynomial(Polynomial data1, Polynomial data2)
+        {
+            //return (data1 - data2).Get();
+            return ((-1)*(data2 - data1)).Get();
         }
     }
 }
