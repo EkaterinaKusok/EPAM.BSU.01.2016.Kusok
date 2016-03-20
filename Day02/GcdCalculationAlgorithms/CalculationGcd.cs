cd99a709ddb8350eea3bc061df4673df37807b8a
EkaterinaKusok <ekaterina.kusok@gmail.com>
2016-03-20 03:41:32 +0300

    refactoring GCD

--- Day02/GcdCalculationAlgorithms/CalculationGcd.cs
+++ Day02/GcdCalculationAlgorithms/CalculationGcd.cs
@@ -1,80 +1,160 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace GcdCalculationAlgorithms
 {
     public class CalculationGcd
     {
-        public static int EuclideanAlgorithm(out TimeSpan workingTime, int first, int second)
-        {
-            if (first < 0 || second < 0)
-            {
-                throw new ArgumentOutOfRangeException(" At least one of the parameters < 0.");
-            }
-            Stopwatch stopWatch = new Stopwatch();
-            try
-            {
-                stopWatch.Start();
-                //* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
-                if (first == 0) return second;
-                if (second == 0) return first;
-                if (first == 1 || second == 1) return 1;
+        public enum Algorithms { Euclidean, Stein };
 
-                while (second != 0)
-                    second = first%(first = second);
-                return first;
-            }
-            finally
-            {
-                stopWatch.Stop();
-                workingTime = stopWatch.Elapsed;
-            }
+        public static int GCD(Algorithms algorithm, int first, int second)
+        {
+            if (algorithm == Algorithms.Euclidean)
+                return EuclideanAlgorithm(first, second);
+            else
+                return SteinAlgorithm(first, second);
         }
 
-        public static int EuclideanAlgorithm(out TimeSpan workingTime, params int[] numbers)
+        public static int GCD(Algorithms algorithm, int first, int second, int third)
+        {
+            return GCD(algorithm, GCD(algorithm, first, second), third);
+        }
+
+        public static int GCD(Algorithms algorithm = Algorithms.Euclidean, params int[] numbers)
         {
             if (numbers == null)
-            {
                 throw new ArgumentNullException();
-            }
             if (numbers.Length < 2)
-            {
                 throw new ArgumentException("The number of values < 2.");
-            }
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            TimeSpan current;
             int result = numbers[0];
             for (int i = 1; i < numbers.Length; i++)
-                result = EuclideanAlgorithm(out current, result, numbers[i]);
+                result = GCD(algorithm, result, numbers[i]);
+            return result;
+        }
+
+        public static int GCD(out TimeSpan workingTime, Algorithms algorithm, int first, int second)
+        {
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            if (algorithm == Algorithms.Euclidean)
+                result = EuclideanAlgorithm(first, second);
+            else
+                result = SteinAlgorithm(first, second);
             stopWatch.Stop();
             workingTime = stopWatch.Elapsed;
             return result;
         }
 
-        public static int SteinAlgorithm(out TimeSpan workingTime, int first, int second)
+        public static int GCD(out TimeSpan workingTime, Algorithms algorithm, int first, int second, int third)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int result = 0;
+            result =GCD(algorithm, GCD(algorithm, first, second), third);
+            stopWatch.Stop();
+            workingTime = stopWatch.Elapsed;
+            return result;
+        }
+
+        public static int GCD(out TimeSpan workingTime, Algorithms algorithm, params int[] numbers)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            if (numbers == null)
+                throw new ArgumentNullException();
+            if (numbers.Length < 2)
+                throw new ArgumentException("The number of values < 2.");
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                result = GCD(algorithm, result, numbers[i]);
+            stopWatch.Stop();
+            workingTime = stopWatch.Elapsed;
+            return result;
+        }
+
+        private static int EuclideanAlgorithm(int first, int second)
         {
             if (first < 0 || second < 0)
-            {
-                throw new ArgumentOutOfRangeException("At least one of the parameters < 0 !");
-            }
-            Stopwatch stopWatch = new Stopwatch();
-            try
-            {
-                stopWatch.Start();
-                if (first == 0) return second;
-                if (second == 0) return first;
-                if (first == second) return first;
-                if (first == 1 || second == 1) return 1;
+                throw new ArgumentOutOfRangeException(" At least one of the parameters < 0.");
+            if (first == 0) return second;
+            if (second == 0) return first;
+            if (first == 1 || second == 1) return 1;
+            while (second != 0)
+                second = first%(first = second);
+            return first;
+        }
 
+        private static int SteinAlgorithm(int first, int second)
+        {
+            if (first < 0 || second < 0)
+                throw new ArgumentOutOfRangeException("At least one of the parameters < 0 !");
+
+            if (first == 0) return second;
+            if (second == 0) return first;
+            if (first == second) return first;
+            if (first == 1 || second == 1) return 1;
+
+            int shift;
+            for (shift = 0; ((first | second) & 1) == 0; ++shift)
+            {
+                first >>= 1;
+                second >>= 1;
+            }
+            while ((first & 1) == 0)
+                first >>= 1;
+            do
+            {
+                while ((second & 1) == 0)
+                    second >>= 1;
+                if (first > second)
+                {
+                    int t = second;
+                    second = first;
+                    first = t;
+                }
+                second = second - first;
+            } while (second != 0);
+            return first << shift;
+        }
+        /*
+        private static int EuclideanAlgorithm(out TimeSpan workingTime, int first, int second)
+        {
+            if (first < 0 || second < 0)
+                throw new ArgumentOutOfRangeException(" At least one of the parameters < 0.");
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            // GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 
+            if (first == 0) result = second;
+            else if (second == 0) result = first;
+            else if (first == 1 || second == 1) result = 1;
+            else
+            {
+                while (second != 0)
+                    second = first % (first = second);
+                result = first;
+            }
+            stopWatch.Stop();
+            workingTime = stopWatch.Elapsed;
+            return result;
+        }
+
+        private static int SteinAlgorithm(out TimeSpan workingTime, int first, int second)
+        {
+            if (first < 0 || second < 0)
+                throw new ArgumentOutOfRangeException("At least one of the parameters < 0 !");
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            if (first == 0) result = second;
+            else if (second == 0) result = first;
+            else if (first == second) result = first;
+            else if (first == 1 || second == 1) result = 1;
+            else
+            {
                 int shift;
-                /* Let shift := lg K, where K is the greatest power of 2
-                  dividing both first and second. */
+                // Let shift := lg K, where K is the greatest power of 2 dividing both first and second. 
                 for (shift = 0; ((first | second) & 1) == 0; ++shift)
                 {
                     first >>= 1;
@@ -82,15 +162,15 @@ namespace GcdCalculationAlgorithms
                 }
                 while ((first & 1) == 0)
                     first >>= 1;
-                /* From here on, first number is always odd. */
+                // From here on, first number is always odd.
                 do
                 {
-                    /* remove all factors of 2 in second number -- they are not common */
-                    /*   note: second number is not zero, so while will terminate */
-                    while ((second & 1) == 0) /* Loop X */
+                    // remove all factors of 2 in second number -- they are not common 
+                    //   note: second number is not zero, so while will terminate 
+                    while ((second & 1) == 0)  
                         second >>= 1;
-                    /* Now first and second numbers are both odd. Swap if necessary so 
-                    first <= second, then set second = second - first (which is even). */
+                    // Now first and second numbers are both odd. Swap if necessary so 
+                    // first <= second, then set second = second - first (which is even). 
                     if (first > second)
                     {
                         int t = second;
@@ -99,37 +179,13 @@ namespace GcdCalculationAlgorithms
                     } // Swap u and v.
                     second = second - first; // Here first >= second.
                 } while (second != 0);
-                /* restore common factors of 2 */
-                return first << shift;
+                // restore common factors of 2 
+                result = first << shift;
             }
-            finally
-            {
-                stopWatch.Stop();
-                workingTime = stopWatch.Elapsed;
-            }
-
-        }
-
-        public static int SteinAlgorithm(out TimeSpan workingTime, params int[] numbers)
-        {
-            if (numbers == null)
-            {
-                throw new ArgumentNullException();
-            }
-            if (numbers.Length < 2)
-            {
-                throw new ArgumentException("The number of values < 2.");
-            }
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            TimeSpan current;
-            int result = numbers[0];
-            for (int i = 1; i < numbers.Length; i++)
-                result = SteinAlgorithm(out current, result, numbers[i]);
             stopWatch.Stop();
             workingTime = stopWatch.Elapsed;
             return result;
         }
-
+        */
     }
 }
--- Day02/GcdCalculationAlgorithmsTests/CalculationGcdTests.cs
+++ Day02/GcdCalculationAlgorithmsTests/CalculationGcdTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using static GcdCalculationAlgorithms.CalculationGcd;
 
@@ -12,36 +9,69 @@ namespace GcdCalculationAlgorithmsTests
     public class CalculationGcdTests
     {
         private TimeSpan _workingTime;
+
         public IEnumerable<TestCaseData> TestData
         {
 
             get
             {
-                yield return new TestCaseData(new int[] { 8, 16}).Returns(8);
-                yield return new TestCaseData(new int[] {8,0}).Returns(8);
-                yield return new TestCaseData(new int[] { 0, 0}).Returns(0);
-                yield return new TestCaseData(new int[] { 1, 3}).Returns(1);
-                yield return new TestCaseData(new int[] { 0, -1}).Throws(typeof(ArgumentOutOfRangeException));
-                yield return new TestCaseData(null).Throws(typeof(ArgumentNullException));
-                yield return new TestCaseData(new int[0]).Throws(typeof(ArgumentException));
-                yield return new TestCaseData( 10 ).Throws(typeof(ArgumentException));
-                yield return new TestCaseData(new int[] { 2, 4, 6}).Returns(2);
-                yield return new TestCaseData(new int[] { 100, 3, 6}).Returns(1);
-                yield return new TestCaseData(new int[] { 13, 26, 0}).Returns(13);
-                yield return new TestCaseData(new int[] { 0, 20, -1}).Throws(typeof(ArgumentOutOfRangeException));
+                yield return new TestCaseData(new int[] {8, 16}).Returns(8);
+                yield return new TestCaseData(new int[] {8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096}).Returns(8);
+                yield return new TestCaseData(new int[] {8, 16, 32}).Returns(8);
+                yield return new TestCaseData(new int[] {1, 3}).Returns(1);
+                yield return new TestCaseData(new int[] {0, -1}).Throws(typeof (ArgumentOutOfRangeException));
+                yield return new TestCaseData(null).Throws(typeof (ArgumentNullException));
+                yield return new TestCaseData(new int[0]).Throws(typeof (ArgumentException));
+                yield return new TestCaseData(10).Throws(typeof (ArgumentException));
+                yield return new TestCaseData(new int[] {2, 4, 6}).Returns(2);
+                yield return new TestCaseData(new int[] {100, 3, 6}).Returns(1);
+                yield return new TestCaseData(new int[] {13, 26, 0}).Returns(13);
+                yield return new TestCaseData(new int[] {0, 20, -1}).Throws(typeof (ArgumentOutOfRangeException));
             }
-        }
-        
-        [Test, TestCaseSource(nameof(TestData))]
-        public double EuclideanAlgorithm_FindGcd2Numbers_WithYield(params int[] data)
-        {
-            return EuclideanAlgorithm(out _workingTime, data);
         }
 
         [Test, TestCaseSource(nameof(TestData))]
-        public double SteinAlgorithm_FindGcd2Numbers_WithYield(params int [] data)
+        public int EuclideanAlgorithm_FindGcd_WithYield(params int[] data)
         {
-            return SteinAlgorithm(out _workingTime, data);
+
+            int result = GCD(out _workingTime, Algorithms.Euclidean, data);
+           // Debug.WriteLine(_workingTime.TotalMilliseconds);
+            return result;
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public int SteinAlgorithm_FindGcd_WithYield(params int[] data)
+        {
+            int result = GCD(out _workingTime, Algorithms.Stein, data);
+            //Debug.WriteLine(_workingTime.TotalMilliseconds);
+            return result;
+        }
+
+        public IEnumerable<TestCaseData> TestData1
+        {
+            get
+            {
+                yield return new TestCaseData(8, 16).Returns(8);
+                yield return new TestCaseData(16, 24).Returns(8);
+                yield return new TestCaseData(86899875, 1777489365).Returns(15);
+                yield return new TestCaseData(1048576, 1073741823).Returns(1);
+                yield return new TestCaseData(0, -1).Throws(typeof (ArgumentOutOfRangeException));
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestData1))]
+        public int EuclideanAlgorithm_FindGcdFor2Numbers_WithYield(int num1, int num2)
+        {
+            int result = GCD(out _workingTime, Algorithms.Euclidean, num1, num2);
+            //Debug.WriteLine(_workingTime.Ticks);
+            return result;
+        }
+        [Test, TestCaseSource(nameof(TestData1))]
+        public int SteinAlgorithm_FindGcdFor2Numbers_WithYield(int num1, int num2)
+        {
+            int result = GCD(out _workingTime, Algorithms.Stein, num1, num2);
+            //Debug.WriteLine(_workingTime.Ticks);
+            return result;
         }
     }
 }
